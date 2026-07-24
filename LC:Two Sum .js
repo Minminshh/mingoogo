e66d81f791bd8b1c64cab3df20485fce016f73ae
Minminshh <minruan1@yahoo.com>
2026-07-23 21:02:47 -0700

    Update LC:Two Sum .js

--- LC:Two Sum .js
+++ LC:Two Sum .js
@@ -3,8 +3,8 @@ Var twoSum = function (nums, target) {
   let numToMemory = new Map();
   for (let i=0; i<nums.length; i++) {
     let complement = target - nums[i];
-    if (complement.has(numToMemory(complement)) {
-      return [complement.get(numToMemory(complement), i)];
+    if (numToMemory.has(complement)) {
+      return [numToMemory.get(complement), i)];
               }
     numToMemory.set(nums[i], i)
   }
